5f884f627173ae3223e935700f44e7cfd54740c4
NgocMinh000 <153832394+NgocMinh000@users.noreply.github.com>
2025-11-29 19:07:25 +0700

    add railway connection

--- my-nestjs-backend-backup/src/project-members/dto/update-member-role.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsEnum, IsNotEmpty } from 'class-validator';
-
-export class UpdateMemberRoleDto {
-  @IsEnum(['viewer', 'member', 'admin'])
-  @IsNotEmpty()
-  role: 'viewer' | 'member' | 'admin';
-}
--- my-nestjs-backend-backup/src/sprints/sprints.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SprintsService } from './sprints.service';
-import { SprintsController } from './sprints.controller';
-
-@Module({
-  providers: [SprintsService],
-  controllers: [SprintsController]
-})
-export class SprintsModule {}
