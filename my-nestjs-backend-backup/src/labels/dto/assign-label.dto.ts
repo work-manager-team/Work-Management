b4d436a93e728f4a150bf01070bd834779b86693
trumcodelord <trumcodelord@users.noreply.github.com>
2025-11-29 19:07:25 +0700

    add railway connection

--- my-nestjs-backend-backup/src/labels/dto/assign-label.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNumber, IsNotEmpty } from 'class-validator';
-
-export class AssignLabelDto {
-  @IsNumber()
-  @IsNotEmpty()
-  labelId: number;
-}
--- my-nestjs-backend-backup/src/tasks/dto/update-task.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaskDto } from './create-task.dto';
-import { IsNumber, IsOptional } from 'class-validator';
-
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-  @IsNumber()
-  @IsOptional()
-  actualHours?: number;
-}
