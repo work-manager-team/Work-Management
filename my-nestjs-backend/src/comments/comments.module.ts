11ef8e2a2110f4b0178675d6fedf32edd5123217
NgocMinh000 <153832394+NgocMinh000@users.noreply.github.com>
2026-01-09 13:39:04 +0700

    auto notify

--- my-nestjs-backend/src/comments/comments.module.ts
+++ my-nestjs-backend/src/comments/comments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   providers: [CommentsService],
   controllers: [CommentsController]
 })
