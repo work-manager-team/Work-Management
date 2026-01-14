d9a7a83b11e0ff9bb51fa07db9d6345349caf837
NgocMinh000 <153832394+NgocMinh000@users.noreply.github.com>
2026-01-14 20:22:21 +0700

    setup websocket notify by Render.com

---
+++ websocket-server/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { NotificationsModule } from './notifications/notifications.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    NotificationsModule,
+  ],
+})
+export class AppModule {}
